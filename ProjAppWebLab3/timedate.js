836f02806063d7066b17def3cab946a45746dbd1
Rumaxx <92017681+Rumaxx@users.noreply.github.com>
2023-10-25 13:14:22 +0200

    comit

--- ProjAppWebLab3/timedate.js
+++ ProjAppWebLab3/timedate.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function odliczanieDaty(){
     var x = new Date();
     var day = x.getDate();
@@ -23,29 +22,3 @@ function odliczanieCzasu(){
     document.getElementById("zegar").innerHTML = hour + ":" + minute + ":" + second;
     setTimeout("odliczanieCzasu()", 1000); 
 }
-=======
-function odliczanieDaty(){
-    var x = new Date();
-    var day = x.getDate();
-    if(day < 10) day = "0" + day;
-    const monthNames=["Styczeń","Luty","Marzec","Kwiecień","Maj","Czerwiec","Lipiec","Sierpień","Wrzesień","Październik","Listopad","Grudzień"];
-    var month = monthNames[x.getMonth()];
-    var year = x.getFullYear()
-
-    document.getElementById("date").innerHTML = day + " " + month + " " + year;
-}
-
-function odliczanieCzasu(){
-    var x = new Date();
-
-    var hour = x.getHours();
-    if(hour < 10) hour = "0" + hour;
-    var minute = x.getMinutes();
-    if(minute < 10) minute = "0" + minute;
-    var second = x.getSeconds();
-    if(second < 10) second = "0" + second;
-
-    document.getElementById("zegar").innerHTML = hour + ":" + minute + ":" + second;
-    setTimeout("odliczanieCzasu()", 1000); 
-}
->>>>>>> 8f1937d27b89d7dafef47ea9ad8039b0a8c7fd2c
---
+++ ProjAppWebLab4/labor_167364_4.php
@@ -0,0 +1,9 @@
+<?
+    $nr_indeksu = '167364';
+    $nr_grupy='4';
+
+    echo 'Dawid Rumiński'.$nr_indeksu.'grupa'.$nrGrupy.'<br/><br/>';
+
+    echo 'Zastosowanie metody include() <br/>';
+
+?>
