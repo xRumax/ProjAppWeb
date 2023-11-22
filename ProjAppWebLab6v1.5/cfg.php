33b795b0e069251a2e82f596b5b4b1f3f9023461
xRumax <167364@student.uwm.edu.pl>
2023-11-22 15:03:11 +0100

    php

---
+++ ProjAppWebLab6v1.5/cfg.php
@@ -0,0 +1,12 @@
+<?php
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $baza = 'gry_strona';
+
+    $link = mysql_connect($dbhost, $dbuser, $dbpass);
+    if(!$link) echo '<b> przerwane połączenie </b>';
+    if(!mysql_select_db($baza)) echo 'nie wybrano bazy';
+
+?>
+
