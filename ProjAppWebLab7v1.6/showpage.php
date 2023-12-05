34fe4566b3c62868e7538800ab3d34448ee14ceb
Rumaxx <92017681+Rumaxx@users.noreply.github.com>
2023-12-05 20:27:49 +0100

    cmt

---
+++ ProjAppWebLab7v1.6/showpage.php
@@ -0,0 +1,18 @@
+<?
+
+function PokazPodstrone($id) {
+    $id_clear = htmlspecialchars($id);
+
+    $query = "SELECT * FROM page_list WHERE id='$id_clear' LIMIT 1";
+    $result = mysql_query($query);
+    $row = mysql_fetch_array($result);
+
+    if(empty($row['id'])) {
+        $web = '[nie_znaleziono_strony]';
+    } else {
+        $web = $row['page_content'];
+    }
+    return $web;
+
+}
+?>
