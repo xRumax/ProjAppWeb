cc525fb91580f64d62e49532a0203b58f6747c11
Rumaxx <92017681+Rumaxx@users.noreply.github.com>
2023-12-06 13:33:20 +0100

    c

--- ProjAppWebLab7v1.6/admin/admin.php
+++ ProjAppWebLab7v1.6/admin/admin.php
@@ -1,18 +1,18 @@
 <?php
-session_start(); // Rozpoczęcie sesji
+session_start();
 
 include('../cfg.php');
 
-// Sprawdzenie czy formularz logowania został wysłany
+
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Sprawdzenie poprawności danych logowania
+
     if(isset($_POST['login_email']) && isset($_POST['login_pass'])) {
         $user_login = $_POST['login_email'];
         $user_pass = $_POST['login_pass'];
 
-        // Sprawdzenie czy dane logowania są poprawne
+
         if($user_login == $login && $user_pass == $pass) {
-            // Ustawienie zmiennej sesyjnej oznaczającej zalogowanie
+
             $_SESSION['logged_in'] = true;
         } else {
             $error_message = "Błędne dane logowania. Spróbuj ponownie.";
@@ -20,20 +20,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-// Obsługa wylogowywania
 if(isset($_GET['action']) && $_GET['action'] == 'logout') {
-    // Zniszczenie sesji
     session_destroy();
-    // Przekierowanie na stronę logowania
     header("Location: admin.php");
     exit();
 }
 
-// Sprawdzenie czy użytkownik jest zalogowany
 if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
-    // Użytkownik jest zalogowany, można wykonywać operacje administracyjne
 
-    // Metoda ListaPodstron()
     function ListaPodstron() {
         global $link;
         $query = "SELECT * FROM page_list ORDER BY id LIMIT 100";
@@ -57,13 +51,13 @@ if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
         }
     }
 
-    // Przycisk wylogowywania
+
     echo '<button style="float:right"><a href="?action=logout" style="text-decoration: none;">Wyloguj</a></button>';
 
-    // Wywołanie metody ListaPodstron()
+
     ListaPodstron();
 } else {
-    // Użytkownik nie jest zalogowany, wyświetl formularz logowania
+
     if(isset($error_message)) {
         echo '<p style="color:red;">'.$error_message.'</p>';
     }
@@ -74,34 +68,32 @@ if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
 function EdytujPodstrone() {
     global $link;
 
-    // Sprawdź, czy przekazano poprawne id podstrony do edycji
+
     if(isset($_POST['edytuj']) && isset($_POST['id_podstrony'])) {
         $id_podstrony = mysqli_real_escape_string($link, $_POST['id_podstrony']);
 
-        // Pobierz dane podstrony z bazy danych
+
         $query = "SELECT * FROM page_list WHERE id = '$id_podstrony'";
         $result = mysqli_query($link, $query);
 
         if($result && mysqli_num_rows($result) > 0) {
             $row = mysqli_fetch_assoc($result);
 
-            // Formularz edycji podstrony
+
             echo '<h2>Edycja Podstrony</h2>';
             echo '<form method="post" action="zapisz_edycje.php">';
             echo '<input type="hidden" name="id_podstrony" value="'.$id_podstrony.'">';
 
-            // Pole dla tytułu
             echo '<label for="tytul">Tytuł:</label>';
             echo '<input type="text" name="tytul" value="'.htmlspecialchars($row['page_title']).'" required>';
 
-            // Pole dla treści strony
+
             echo '<label for="tresc">Treść:</label>';
             echo '<textarea name="tresc" rows="8" required>'.htmlspecialchars($row['page_content']).'</textarea>';
 
-            // Pole checkbox dla aktywności strony
             echo '<label>Aktywna: <input type="checkbox" name="aktywna" '.($row['active'] ? 'checked' : '').'></label>';
 
-            // Przycisk zapisu
+
             echo '<input type="submit" name="zapisz" value="Zapisz">';
             echo '</form>';
         } else {
@@ -111,7 +103,7 @@ function EdytujPodstrone() {
 }
 
 
-// Metoda FormularzLogowania()
+
 function FormularzLogowania() {
     $form = '
         <div class="logowanie">
